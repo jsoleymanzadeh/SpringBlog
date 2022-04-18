8bc72235d5db226dece6553055dfa9fce759452e
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-04-18 16:26:04 -0500

    finished views dice roll exercise

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
+    @ResponseBody
+    public String hello() {
+        return "Hello, world!";
+    }
+
     @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
     @ResponseBody
-    public String hello(@PathVariable String name) {
+    public String helloPerson(@PathVariable String name) {
         return "Hello " + name + "!";
     }
 }
