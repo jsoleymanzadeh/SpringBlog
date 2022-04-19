b71cc3f7b20f3fd8d70b53bea5d9b74e689b647c
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-04-19 09:30:55 -0500

    finished view exercises

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,23 +1,31 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostController {
     @RequestMapping(path = "/posts", method = RequestMethod.GET)
-    @ResponseBody
-    public String postsIndex() {
-        return "posts index page";
+    public String postsIndex(Model model) {
+        ArrayList<Post> allPosts = new ArrayList<>();
+        allPosts.add(new Post("title1", "body1"));
+        allPosts.add(new Post("title2", "body2"));
+        model.addAttribute("allPosts", allPosts);
+        return "posts/index";
     }
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
-    @ResponseBody
-    public String singlePost(@PathVariable int id) {
-        return "view an individual post";
+    public String singlePost(@PathVariable int id, Model model) {
+        String title = "Test Title";
+        String body = "Test Body";
+        model.addAttribute("post", new Post(title, body));
+        return "posts/show";
     }
 
     @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
