715b7c28c5ac2ca7df2826c72dc1bfc0be22e50f
Jasper Soleymanzadeh <jaspersoleymanzadeh@gmail.com>
2022-04-21 09:52:03 -0500

    finished services exercise

---
+++ src/main/java/com/codeup/springblog/EmailService.java
@@ -0,0 +1,29 @@
+package com.codeup.springblog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service("mailService")
+public class EmailService {
+    @Autowired
+    public JavaMailSender emailSender;
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(Post post, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(post.getUser().getEmail());
+        message.setSubject(subject);
+        message.setText(body);
+        try {
+            this.emailSender.send(message);
+        } catch (MailException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -6,10 +6,12 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class PostController {
+    private final EmailService emailService;
     private final PostRepository postDao;
     private final UserRepository userDao;
 
-    public PostController(PostRepository postDao, UserRepository userDao) {
+    public PostController(EmailService emailService, PostRepository postDao, UserRepository userDao) {
+        this.emailService = emailService;
         this.postDao = postDao;
         this.userDao = userDao;
     }
@@ -35,14 +37,17 @@ public class PostController {
     @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
     public String createPost(@ModelAttribute Post post) {
         post.setUser(userDao.getUserById(1));
-        postDao.save(post);
+        post = postDao.save(post);
+        emailService.prepareAndSend(post, "New Post", "Your post titled \"" + post.getTitle() + "\" is live!\nSee it here: http://localhost:8080/posts/" + post.getId());
         return "redirect:/posts";
     }
+
     @RequestMapping(path = "/posts/{id}/edit", method = RequestMethod.GET)
     public String editPostForm(@PathVariable long id, Model model) {
         model.addAttribute("post", postDao.getPostById(id));
         return "posts/edit";
     }
+
     @RequestMapping(path = "/posts/edit", method = RequestMethod.POST)
     public String editPost(@ModelAttribute Post post) {
         postDao.save(post);
